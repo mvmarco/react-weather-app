06476d13c69b82ac3dd22d53d06f2c6b1c7f8896
marco corapi <marco.corapi@outlook.it>
2021-04-27 19:17:52 +0200

    event and input search

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import axios from 'axios';
 import {useEffect, useState} from 'react';
 
 function App() {
+
+  // here we save our weather data in a React state
+  // by default the data is null until the data is fetched
+  const [weather, setWeather] = useState(null);
+
+  // use effect when page load display data
   useEffect(() => {
     const location = axios.get(
       'http://api.weatherapi.com/v1/current.json?key=fb454c4007ae4b63b83154929212704&q=copenhagen&aqi=no'
     ).then((data) => {
-      console.log(data)
+        console.log(data.data)
+        // data saved into a react state
+        setWeather(data.data)
     }).catch(err => console.log(err, "Sorry something went wrong, try later"));
   },[])
+  // another state to let the user search the city
+  const [input, setInput] = useState('');
+  // here we say, if the weather is present then show it otherwise not, to prevent errors
   return (
     <div>
-      <h1>Weather App</h1>
+      {weather && (
+        <div>
+
+          <div className="search">
+            <input type="text"/>
+            <button>Search</button>
+          </div>
+
+          <div className="weather-info">
+            <h1>{weather.location.country}</h1>
+            <h2>{weather.location.region}</h2>
+            <div className="condition">
+              <h3>{weather.current.condition.text}</h3>
+              <img src={weather.current.condition.icon} alt="weather icon" />
+              <h3>{weather.current.temp_c} Degree Celsius</h3>
+            </div>
+          </div>
+
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
