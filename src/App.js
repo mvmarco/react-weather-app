36cd960299f6c941c173c82aec657a4c3dfe7185
marco corapi <marco.corapi@outlook.it>
2021-04-27 18:48:53 +0200

    axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
+
+// by default when the page reloads we fetch Kbh data by default with UseEffect
+import {useEffect, useState} from 'react';
+
 function App() {
+  useEffect(() => {
+    const location = axios.get(
+      'http://api.weatherapi.com/v1/current.json?key=fb454c4007ae4b63b83154929212704&q=copenhagen&aqi=no'
+    ).then((data) => {
+      console.log(data)
+    }).catch(err => console.log(err, "Sorry something went wrong, try later"));
+  },[])
   return (
     <div>
-
+      <h1>Weather App</h1>
     </div>
   );
 }
