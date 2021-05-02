5cacf512c4e6609b68bd237769dc5f6771465101
marco corapi <marco.corapi@outlook.it>
2021-05-02 12:57:13 +0200

    basic style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
-
 // by default when the page reloads we fetch Kbh data by default with UseEffect
 import {useEffect, useState} from 'react';
-
+// import styled components
+import styled from "styled-components";
+// img
+import img from './img/sky.jpg'
 function App() {
 
   // here we save our weather data in a React state
   // by default the data is null until the data is fetched
   const [weather, setWeather] = useState(null);
+  
+  // another state to let the user search the city
+  const [input, setInput] = useState('');
 
   // use effect when page load display data
   useEffect(() => {
@@ -20,40 +25,74 @@ function App() {
     }).catch(err => console.log(err, "Sorry something went wrong, try later"));
   },[])
 
-  // another state to let the user search the city
-  const [input, setInput] = useState('');
-
   // event
   const handleInputWeather = (e) => {
     console.log(e.target.value);
     setInput(e.target.value);
   }
 
+  const searchWeather = () => {
+   axios.get(
+     `http://api.weatherapi.com/v1/current.json?key=fb454c4007ae4b63b83154929212704&q=${input}&aqi=no`
+   ).then((data) => {
+     setWeather(data.data)
+   }).catch(err => console.log(err, "City not found, try again"));
+  }
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      searchWeather();
+      console.log('trying')
+    }
+  };
   // here we say, if the weather is present then show it otherwise not, to prevent errors
   return (
-    <div>
+    <AppStyle>
       {weather && (
-        <div>
-
+        <WeatherContainer>
           <div className="search">
-            <input onChange={handleInputWeather} type="text"/>
-            <button>Search</button>
+            <input value={input} type="text" onChange={handleInputWeather} onKeyPress={handleKeypress} />
+            <button onClick={searchWeather} type="submit">
+              Search
+            </button>
           </div>
 
           <div className="weather-info">
             <h1>{weather.location.country}</h1>
-            <h2>{weather.location.region}</h2>
+            <h2>{weather.location.name}</h2>
             <div className="condition">
-              <h3>{weather.current.condition.text}</h3>
+              <h3>Sky: {weather.current.condition.text}</h3>
               <img src={weather.current.condition.icon} alt="weather icon" />
-              <h3>{weather.current.temp_c} Degree Celsius</h3>
+              <h3>Temperature: {weather.current.temp_c} Degree Celsius</h3>
+              <h3>Feels like: {weather.current.feelslike_c} Degree Celsius</h3>
+              <h3>Wind: {weather.current.wind_kph} km/h </h3>
             </div>
           </div>
-
-        </div>
+        </WeatherContainer>
       )}
-    </div>
+    </AppStyle>
   );
 };
 
+
+const AppStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-image: url(${img});
+  background-size: 100% 100%;
+`;
+const WeatherContainer = styled.div`
+  margin: 2.5rem;
+  padding: 2.5rem;
+  border-radius: 20px;
+  background-color:rgba(239, 255, 255, 0.5);
+  @media (max-width: 518px) {
+    padding: 1.5rem;
+    margin: 1.5rem;
+  }
+`;
 export default App;
